e3b42898258e456ee502941be9f4c65d63d8a7b0
unknown <fateh_djehinet@outlook.com>
2021-08-27 17:36:00 +0100

    setting Material Ui up

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
+import {
+  Button,
+  Switch,
+  TextInput,
+  Paragraph,
+  Appbar,
+  Title,
+} from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Avatar, Card } from "react-native-paper";
+
+const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log("Pressed")}
+      >
+        Press me
+      </Button>
+      <Card>
+        <Card.Title
+          title="Card Title"
+          subtitle="Card Subtitle"
+          left={LeftContent}
+        />
+        <Card.Content>
+          <Title>Card title</Title>
+          <Paragraph>Card content</Paragraph>
+        </Card.Content>
+        <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
+      <Appbar style={styles.bottom}>
+        <Appbar.Action
+          icon="archive"
+          onPress={() => console.log("Pressed archive")}
+        />
+        <Appbar.Action
+          icon="mail"
+          onPress={() => console.log("Pressed mail")}
+        />
+        <Appbar.Action
+          icon="label"
+          onPress={() => console.log("Pressed label")}
+        />
+        <Appbar.Action
+          icon="delete"
+          onPress={() => console.log("Pressed delete")}
+        />
+      </Appbar>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bottom: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
